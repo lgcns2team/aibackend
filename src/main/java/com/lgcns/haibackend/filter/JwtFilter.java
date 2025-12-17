3a830bae66a1a2676ad36834eb1c81172aea650c
Hong9915 <hhj4498@naver.com>
2025-12-17 14:49:01 +0900

    feat : debate

--- src/main/java/com/lgcns/haibackend/filter/JwtFilter.java
+++ src/main/java/com/lgcns/haibackend/filter/JwtFilter.java
@@ -129,6 +129,7 @@ public class JwtFilter implements Filter {
                 path.startsWith("/api/main-event/detail") ||
                 path.startsWith("/api/user/signup") ||
                 path.startsWith("/api/user/login") ||
+                path.startsWith("/api/debate") || // 개발 중 필요시 주석해제
                 path.startsWith("/chat/room") ||
                 path.startsWith("/ws-stomp") ||
                 path.startsWith("/favicon.ico") ||
